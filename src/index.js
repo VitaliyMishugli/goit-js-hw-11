5e2cb47cab0539bcfb730f61f502ca7f078dbef5
Vitaliy Mishugli <vmishughli@gmail.com>
2022-09-26 20:29:21 +0100

    End of content

--- src/index.js
+++ src/index.js
@@ -24,11 +24,14 @@ function onSearch(e) {
   e.preventDefault();
   refs.gallery.innerHTML = '';
   // refs.loadMoreBtn.disabled = false;
-  refs.loadMoreBtn.classList.remove('is-hidden');
+  
   const trg = e.currentTarget;
-  const search = trg.elements.searchQuery;
-  if (search.value !== '') {
-    photoApiService.query = search.value.trim();
+  let search = trg.elements.searchQuery;
+  search = search.value.trim();
+  
+  if (search !== '') {
+    refs.loadMoreBtn.classList.remove('is-hidden');
+    photoApiService.query = search;
     photoApiService.resetPage();
     photoRender();
     Notiflix.Notify.success(`✅ Hooray! We found totalHits images.`);
@@ -40,10 +43,12 @@ function onSearch(e) {
 }
 
 function onLoadMore() {
-  refs.loadMoreBtn.disabled = true;
+  // refs.loadMoreBtn.disabled = true;
+  refs.loadMoreBtn.classList.add('is-hidden');
   photoApiService.fetchPhotos();
   photoRender();
-  refs.loadMoreBtn.disabled = false;
+  // refs.loadMoreBtn.disabled = false;
+  refs.loadMoreBtn.classList.remove('is-hidden');
 }
 
 function photoRender() {
@@ -52,15 +57,70 @@ function photoRender() {
     .then(response => {
       photoApiService.pageIncrement();
       let result = response.data.hits;
-      // console.log(result[0].largeImageURL);
+
       if (result.length === 0) {
+        console.log(result.length);
         Notiflix.Notify.failure(
           `Sorry, there are no images matching your search query. Please try again.`
         );
       }
-      let render = result
-        .map(item => {
-          return `<div class="photo-card" class="gallery__item">
+
+      // else if (result.length < 40) {
+      //   console.log(result.length);
+      //   refs.loadMoreBtn.classList.add('is-hidden');
+      //   Notiflix.Notify.failure(
+      //     `We're sorry, but you've reached the end of search results.`
+      //   );
+
+        // =====
+      //   let render = result
+      //     .map(item => {
+      //       return `<div class="photo-card" class="gallery__item">
+
+      //   <a href="${item.largeImageURL}"    class="gallery__item">
+      //     <img
+      //     src="${item.webformatURL}"           
+      //     class="gallery__image"
+      //     width="150"
+      //     alt="${item.tags}"
+      //     loading="lazy" />
+      //   </a>
+
+      //   <div class="info">
+      //     <div class="info-item">
+      //       <p class="item-name"><b>Likes</b></p>
+      //       <p class="item-value">${item.likes}</p>
+      //     </div>
+      //     <div class="info-item">
+      //       <p class="item-name"><b>Views</b></p>
+      //       <p class="item-value">${item.views}</p>
+      //     </div>
+      //     <div class="info-item">
+      //       <p class="item-name"><b>Comments</b></p>
+      //       <p class="item-value">${item.comments}</p>
+      //     </div>         
+      //     <div class="info-item">
+      //       <p class="item-name"><b>Downloads</b></p>
+      //       <p class="item-value">${item.downloads}</p>
+      //     </div>
+      //   </div>
+      // </div>`;
+      //     })
+      //     .join('');
+
+      //   refs.gallery.innerHTML += render;
+      //   const lightbox = new SimpleLightbox('.gallery a', {
+      //     captionsData: 'alt',
+      //     captionDelay: 250,
+      //   });
+        // =====
+      // }
+
+      // else if (result.length === 40) {
+        console.log(result.length);
+        let render = result
+          .map(item => {
+            return `<div class="photo-card" class="gallery__item">
 
         <a href="${item.largeImageURL}"    class="gallery__item">
           <img
@@ -90,14 +150,16 @@ function photoRender() {
           </div>
         </div>
       </div>`;
-        })
-        .join('');
+          })
+          .join('');
+
+        refs.gallery.innerHTML += render;
+        const lightbox = new SimpleLightbox('.gallery a', {
+          captionsData: 'alt',
+          captionDelay: 250,
+        });
+      // }
       
-      refs.gallery.innerHTML += render;
-      const lightbox = new SimpleLightbox('.gallery a', {
-        captionsData: 'alt',
-        captionDelay: 250,
-      });
     })
     .catch(error => {
       console.log(error);
