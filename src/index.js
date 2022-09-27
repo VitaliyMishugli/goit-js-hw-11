2d29cc24c431488e6db50c32b3b8305298e5ffa6
Vitaliy Mishugli <vmishughli@gmail.com>
2022-09-27 21:00:08 +0100

    final

--- src/index.js
+++ src/index.js
@@ -6,9 +6,12 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const DEBOUNCE_DELAY = 300;
-
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 const photoApiService = new PhotosApiService();
-
+let page = photoApiService.page;
 const refs = {
   form: document.querySelector('#search-form'),
   gallery: document.querySelector('.gallery'),
@@ -24,6 +27,7 @@ function onSearch(e) {
   e.preventDefault();
   refs.gallery.innerHTML = '';
   // refs.loadMoreBtn.disabled = false;
+  page += 1;
   
   const trg = e.currentTarget;
   let search = trg.elements.searchQuery;
@@ -35,6 +39,7 @@ function onSearch(e) {
     photoApiService.resetPage();
     photoRender();
     Notiflix.Notify.success(`✅ Hooray! We found totalHits images.`);
+   
   } else {
     Notiflix.Notify.failure(
       `Sorry, there are no images matching your search query. Please try again.`
@@ -43,7 +48,7 @@ function onSearch(e) {
 }
 
 function onLoadMore() {
-  // refs.loadMoreBtn.disabled = true;
+  page += 1;
   refs.loadMoreBtn.classList.add('is-hidden');
   photoApiService.fetchPhotos();
   photoRender();
@@ -52,72 +57,30 @@ function onLoadMore() {
 }
 
 function photoRender() {
+  photoApiService.pageIncrement();
   photoApiService
     .fetchPhotos()
     .then(response => {
-      photoApiService.pageIncrement();
+      // console.log(response);
+      const tHits = response.data.totalHits;
       let result = response.data.hits;
-
+      const totalPages = Math.ceil(tHits / result.length);
+      // console.log(totalPages);
       if (result.length === 0) {
-        console.log(result.length);
+        refs.loadMoreBtn.classList.add('is-hidden');
         Notiflix.Notify.failure(
           `Sorry, there are no images matching your search query. Please try again.`
         );
       }
 
-      // else if (result.length < 40) {
-      //   console.log(result.length);
-      //   refs.loadMoreBtn.classList.add('is-hidden');
-      //   Notiflix.Notify.failure(
-      //     `We're sorry, but you've reached the end of search results.`
-      //   );
+      else if (page === totalPages) {
+        // console.log(result.length);
+        refs.loadMoreBtn.classList.add('is-hidden');
+        Notiflix.Notify.failure(
+          `We're sorry, but you've reached the end of search results.`
+        );
 
         // =====
-      //   let render = result
-      //     .map(item => {
-      //       return `<div class="photo-card" class="gallery__item">
-
-      //   <a href="${item.largeImageURL}"    class="gallery__item">
-      //     <img
-      //     src="${item.webformatURL}"           
-      //     class="gallery__image"
-      //     width="150"
-      //     alt="${item.tags}"
-      //     loading="lazy" />
-      //   </a>
-
-      //   <div class="info">
-      //     <div class="info-item">
-      //       <p class="item-name"><b>Likes</b></p>
-      //       <p class="item-value">${item.likes}</p>
-      //     </div>
-      //     <div class="info-item">
-      //       <p class="item-name"><b>Views</b></p>
-      //       <p class="item-value">${item.views}</p>
-      //     </div>
-      //     <div class="info-item">
-      //       <p class="item-name"><b>Comments</b></p>
-      //       <p class="item-value">${item.comments}</p>
-      //     </div>         
-      //     <div class="info-item">
-      //       <p class="item-name"><b>Downloads</b></p>
-      //       <p class="item-value">${item.downloads}</p>
-      //     </div>
-      //   </div>
-      // </div>`;
-      //     })
-      //     .join('');
-
-      //   refs.gallery.innerHTML += render;
-      //   const lightbox = new SimpleLightbox('.gallery a', {
-      //     captionsData: 'alt',
-      //     captionDelay: 250,
-      //   });
-        // =====
-      // }
-
-      // else if (result.length === 40) {
-        console.log(result.length);
         let render = result
           .map(item => {
             return `<div class="photo-card" class="gallery__item">
@@ -154,10 +117,53 @@ function photoRender() {
           .join('');
 
         refs.gallery.innerHTML += render;
-        const lightbox = new SimpleLightbox('.gallery a', {
-          captionsData: 'alt',
-          captionDelay: 250,
-        });
+        // const lightbox = new SimpleLightbox('.gallery a', {
+        //   captionsData: 'alt',
+        //   captionDelay: 250,
+        // });
+        lightbox.refresh();
+        // =====
+      }
+
+      // else if (totalPages === 13) {
+        // console.log(result.length);
+        let render = result
+          .map(item => {
+            return `<div class="photo-card" class="gallery__item">
+
+        <a href="${item.largeImageURL}"    class="gallery__item">
+          <img
+          src="${item.webformatURL}"           
+          class="gallery__image"
+          width="150"
+          alt="${item.tags}"
+          loading="lazy" />
+        </a>
+
+        <div class="info">
+          <div class="info-item">
+            <p class="item-name"><b>Likes</b></p>
+            <p class="item-value">${item.likes}</p>
+          </div>
+          <div class="info-item">
+            <p class="item-name"><b>Views</b></p>
+            <p class="item-value">${item.views}</p>
+          </div>
+          <div class="info-item">
+            <p class="item-name"><b>Comments</b></p>
+            <p class="item-value">${item.comments}</p>
+          </div>         
+          <div class="info-item">
+            <p class="item-name"><b>Downloads</b></p>
+            <p class="item-value">${item.downloads}</p>
+          </div>
+        </div>
+      </div>`;
+          })
+          .join('');
+
+      refs.gallery.innerHTML += render;
+      lightbox.refresh();
       // }
       
     })
