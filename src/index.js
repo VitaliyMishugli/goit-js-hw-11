01c8bc283a2f530a5b81bafa4b528c0b69e09fe3
Vitaliy Mishugli <vmishughli@gmail.com>
2022-09-28 19:03:52 +0100

    Edits done(28.09.2022)

--- src/index.js
+++ src/index.js
@@ -11,7 +11,8 @@ const lightbox = new SimpleLightbox('.gallery a', {
   captionDelay: 250,
 });
 const photoApiService = new PhotosApiService();
-let page = photoApiService.page;
+let page = 0;
+let totalPages = 0;
 const refs = {
   form: document.querySelector('#search-form'),
   gallery: document.querySelector('.gallery'),
@@ -20,155 +21,141 @@ const refs = {
 
 refs.form.addEventListener('submit', onSearch);
 refs.loadMoreBtn.addEventListener('click', onLoadMore);
-// refs.loadMoreBtn.disabled = true;
 refs.loadMoreBtn.classList.add('is-hidden');
 
 function onSearch(e) {
   e.preventDefault();
   refs.gallery.innerHTML = '';
-  // refs.loadMoreBtn.disabled = false;
-  page += 1;
-  
   const trg = e.currentTarget;
   let search = trg.elements.searchQuery;
   search = search.value.trim();
   
-  if (search !== '') {
-    refs.loadMoreBtn.classList.remove('is-hidden');
-    photoApiService.query = search;
-    photoApiService.resetPage();
-    photoRender();
-    Notiflix.Notify.success(`✅ Hooray! We found totalHits images.`);
-   
-  } else {
+  if (search === '') {
     Notiflix.Notify.failure(
       `Sorry, there are no images matching your search query. Please try again.`
     );
+    return;   
   }
+   photoApiService.query = search;
+   photoApiService.resetPage();
+   photoRender();
+   Notiflix.Notify.success(`✅ Hooray! We found totalHits images.`);
 }
 
 function onLoadMore() {
   page += 1;
   refs.loadMoreBtn.classList.add('is-hidden');
-  photoApiService.fetchPhotos();
   photoRender();
-  // refs.loadMoreBtn.disabled = false;
   refs.loadMoreBtn.classList.remove('is-hidden');
 }
 
 function photoRender() {
-  photoApiService.pageIncrement();
   photoApiService
     .fetchPhotos()
     .then(response => {
+      page = photoApiService.page;
       // console.log(response);
+      // console.log(page);
       const tHits = response.data.totalHits;
       let result = response.data.hits;
       const totalPages = Math.ceil(tHits / result.length);
-      // console.log(totalPages);
+      console.log(totalPages);
+      refs.loadMoreBtn.classList.remove('is-hidden');
       if (result.length === 0) {
         refs.loadMoreBtn.classList.add('is-hidden');
         Notiflix.Notify.failure(
           `Sorry, there are no images matching your search query. Please try again.`
         );
       }
-
+      // console.log(page);
       else if (page === totalPages) {
-        // console.log(result.length);
         refs.loadMoreBtn.classList.add('is-hidden');
         Notiflix.Notify.failure(
           `We're sorry, but you've reached the end of search results.`
         );
+        photoMarkup(result);
+        //     .map(item => {
+        //       return `<div class="photo-card" class="gallery__item">
 
-        // =====
-        let render = result
-          .map(item => {
-            return `<div class="photo-card" class="gallery__item">
+        //   <a href="${item.largeImageURL}"    class="gallery__item">
+        //     <img
+        //     src="${item.webformatURL}"
+        //     class="gallery__image"
+        //     width="150"
+        //     alt="${item.tags}"
+        //     loading="lazy" />
+        //   </a>
 
-        <a href="${item.largeImageURL}"    class="gallery__item">
-          <img
-          src="${item.webformatURL}"           
-          class="gallery__image"
-          width="150"
-          alt="${item.tags}"
-          loading="lazy" />
-        </a>
+        //   <div class="info">
+        //     <div class="info-item">
+        //       <p class="item-name"><b>Likes</b></p>
+        //       <p class="item-value">${item.likes}</p>
+        //     </div>
+        //     <div class="info-item">
+        //       <p class="item-name"><b>Views</b></p>
+        //       <p class="item-value">${item.views}</p>
+        //     </div>
+        //     <div class="info-item">
+        //       <p class="item-name"><b>Comments</b></p>
+        //       <p class="item-value">${item.comments}</p>
+        //     </div>
+        //     <div class="info-item">
+        //       <p class="item-name"><b>Downloads</b></p>
+        //       <p class="item-value">${item.downloads}</p>
+        //     </div>
+        //   </div>
+        // </div>`;
+        //     })
+        //     .join('');
 
-        <div class="info">
-          <div class="info-item">
-            <p class="item-name"><b>Likes</b></p>
-            <p class="item-value">${item.likes}</p>
-          </div>
-          <div class="info-item">
-            <p class="item-name"><b>Views</b></p>
-            <p class="item-value">${item.views}</p>
-          </div>
-          <div class="info-item">
-            <p class="item-name"><b>Comments</b></p>
-            <p class="item-value">${item.comments}</p>
-          </div>         
-          <div class="info-item">
-            <p class="item-name"><b>Downloads</b></p>
-            <p class="item-value">${item.downloads}</p>
-          </div>
-        </div>
-      </div>`;
-          })
-          .join('');
+        //   refs.gallery.innerHTML += render;
 
-        refs.gallery.innerHTML += render;
-        // const lightbox = new SimpleLightbox('.gallery a', {
-        //   captionsData: 'alt',
-        //   captionDelay: 250,
-        // });
-        lightbox.refresh();
+        //   lightbox.refresh();
         // =====
       }
-
-      // else if (totalPages === 13) {
-        // console.log(result.length);
-        let render = result
-          .map(item => {
-            return `<div class="photo-card" class="gallery__item">
-
-        <a href="${item.largeImageURL}"    class="gallery__item">
-          <img
-          src="${item.webformatURL}"           
-          class="gallery__image"
-          width="150"
-          alt="${item.tags}"
-          loading="lazy" />
-        </a>
-
-        <div class="info">
-          <div class="info-item">
-            <p class="item-name"><b>Likes</b></p>
-            <p class="item-value">${item.likes}</p>
-          </div>
-          <div class="info-item">
-            <p class="item-name"><b>Views</b></p>
-            <p class="item-value">${item.views}</p>
-          </div>
-          <div class="info-item">
-            <p class="item-name"><b>Comments</b></p>
-            <p class="item-value">${item.comments}</p>
-          </div>         
-          <div class="info-item">
-            <p class="item-name"><b>Downloads</b></p>
-            <p class="item-value">${item.downloads}</p>
-          </div>
-        </div>
-      </div>`;
-          })
-          .join('');
-
-      refs.gallery.innerHTML += render;
-      lightbox.refresh();
-      // }
-      
+      photoMarkup(result);
+      photoApiService.pageIncrement();
     })
     .catch(error => {
       console.log(error);
     });
 }
 
+function photoMarkup(result) {
+  let render = result
+    .map(item => {
+      return `<div class="photo-card" class="gallery__item">
+        <a href="${item.largeImageURL}"    class="gallery__item">
+          <img
+          src="${item.webformatURL}"           
+          class="gallery__image"
+          width="150"
+          alt="${item.tags}"
+          loading="lazy" />
+        </a>
+
+        <div class="info">
+          <div class="info-item">
+            <p class="item-name"><b>Likes</b></p>
+            <p class="item-value">${item.likes}</p>
+          </div>
+          <div class="info-item">
+            <p class="item-name"><b>Views</b></p>
+            <p class="item-value">${item.views}</p>
+          </div>
+          <div class="info-item">
+            <p class="item-name"><b>Comments</b></p>
+            <p class="item-value">${item.comments}</p>
+          </div>         
+          <div class="info-item">
+            <p class="item-name"><b>Downloads</b></p>
+            <p class="item-value">${item.downloads}</p>
+          </div>
+        </div>
+      </div>`;
+    })
+    .join('');
+
+  refs.gallery.insertAdjacentHTML('beforeend', render);
+  lightbox.refresh();
+}
